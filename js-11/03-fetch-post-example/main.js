fc38537124f333bbe98e24b4093f44cd8e88cef9
Adrian-Moldovan <ma.moldovan@gmail.com>
2023-08-27 12:20:00 +0300

    Adds js 11 - POST quiz api example

---
+++ js-11/03-fetch-post-example/main.js
@@ -0,0 +1,27 @@
+/**
+ * EXAMPLE OF A POST REQUEST
+ */
+const url = "http://quiz-api.siit.ro/api/register";
+
+let headers = {
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+};
+
+let body = {
+    "email": "siit.frontend.41@siit.ro",
+    "name": "Grupa 41 frontend",
+    "password": "41-rules",
+    "password_confirm": "41-rules"
+}
+
+fetch(url, {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+    },
+    body: JSON.stringify(body),
+})
+    .then(response => response.json())
+    .then(data => console.log(data));
