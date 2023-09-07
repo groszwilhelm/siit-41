3890df5248ef155d2445c87384242c4dca65b709
groszwi <groszwi@bisnode.com>
2023-09-07 21:14:35 +0300

    adds react app

---
+++ react-movies/src/App.js
@@ -0,0 +1,34 @@
+import logo from './logo.svg';
+import './App.css';
+import Movie from './movies/Movie';
+
+const greeting = 'Hello from JS';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p style={{ backgroundColor: "red" }}>
+          Edit <code>src/App.js</code> and save to reload.
+          {greeting}
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+
+      <Movie />
+
+      <input type="text" id="username"></input>
+      <label htmlFor="username"></label>
+    </div>
+  );
+}
+
+export default App;
